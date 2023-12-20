311f83d87d9246fb366e43869fc12d8ee7b83aeb
Jens <jens@cetg.dk>
2023-12-20 23:40:00 +0100

    Can now construct xml strings

---
+++ 011_VejlederTest/CreateXml/Classes.cs
@@ -0,0 +1,73 @@
+namespace XmlTest
+{
+    public static class XmlNode
+    {
+        public static string XmlString(string name, Dictionary<string, string> atts, string value)
+        {
+            string output = $"<{name} ";
+            foreach (KeyValuePair<string, string> att in atts)
+            {
+                output += $"{att.Key}=\"{att.Value}\" ";
+            }
+            output.Remove(output.Count() - 1);
+            output += $">{value}</{name}>";
+            return output;
+        }
+        public static string XmlString(string name, Dictionary<string, string> atts)
+        {
+            string output = $"<{name} ";
+            foreach (KeyValuePair<string, string> att in atts)
+            {
+                output += $"{att.Key}=\"{att.Value}\" ";
+            }
+            output.Remove(output.Count() - 1);
+            output += $"/>";
+            return output;
+        }
+        public static string XmlString(string name, string value)
+        {
+            string output = $"<{name}>{value}</{name}>";
+            return output;
+        }
+        public static string XmlString(string name, List<string> vals)
+        {
+            string output = $"<{name}>";
+            foreach (string val in vals)
+            {
+                output += val;
+            }
+            output += $"</{name}>";
+            return output;
+        }
+        public static string XmlString(string name, Dictionary<string, string> atts, List<string> vals)
+        {
+            string output = $"<{name} ";
+            foreach (KeyValuePair<string, string> att in atts)
+            {
+                output += $"{att.Key}=\"{att.Value}\" ";
+            }
+            output.Remove(output.Count() - 1);
+            output += ">";
+            foreach (string val in vals)
+            {
+                output += val;
+            }
+            output += $"</{name}>";
+            return output;
+        }
+    }
+}
+
+// <row r="1" spans="1:3" x14ac:dyDescent="0.25">
+//     <c r="A1" t="s">
+//         <v>3</v>
+//     </c>
+//     <c r="B1" t="s">
+//         <v>4</v>
+//     </c>
+//     <c r="C1" t="s">
+//         <v>5</v>
+//     </c>
+// </row>
+
+// <selection activeCell="K27" sqref="K27"/>}
--- 011_VejlederTest/CreateXml/Program.cs
+++ 011_VejlederTest/CreateXml/Program.cs
@@ -1,9 +1,50 @@
-﻿public static class XmlNode
+﻿using System.Data;
+using System.Globalization;
+using XmlTest;
+
+List<char> alphabet = new List<char> { 'A', 'B', 'C', 'D', 'E' };
+int cols = 3;
+int rows = 10;
+List<string> cells = new List<string>();
+Dictionary<string, string> atts = new Dictionary<string, string> ();
+string output = "";
+
+for (int r = 1; r < rows + 1; r++)
 {
-    public static string XmlString(string name, Dictionary<string, string> att, string value) {
-
+    cells.Clear();
+    for (int c = 0; c < cols; c++)
+    {
+        atts.Clear();
+        string val = XmlNode.XmlString("v", (2 * c + 1).ToString());
+        if (c < 2)
+        {
+            atts.Add("r", alphabet[c] + r.ToString());
+            atts.Add("t", "s");
+            cells.Add(XmlNode.XmlString("c", atts, val));
+        }
+        else
+        {
+            atts.Add("r", alphabet[c] + r.ToString());
+            cells.Add(XmlNode.XmlString("c", atts, val));
+        }
+        
     }
-    public static string XmlString(string name, Dictionary<string, string> att) {
-
-    }
+    atts.Clear();
+    atts.Add("r", r.ToString());
+    atts.Add("spans", $"1:{cols}");
+    atts.Add("x14ac:dyDescent", "0.25");
+    output += XmlNode.XmlString("row", atts, cells);
 }
+Console.WriteLine(output);
+
+// <row r="2" spans="1:3" x14ac:dyDescent="0.25">
+//     <c r="A2" t="s">
+//         <v>6</v>
+//     </c>
+//     <c r="B2" t="s">
+//         <v>7</v>
+//     </c>
+//     <c r="C2">
+//         <v>1</v>
+//     </c>
+// </row>
