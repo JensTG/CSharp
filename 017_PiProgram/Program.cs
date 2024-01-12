894d6ca156d068d481f6c6c76b4f6256146d3c1e
Jens <jens@cetg.dk>
2024-01-12 09:06:00 +0100

    (A)Sync

---
+++ 017_PiProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿double pi = 0.0d;
+bool sign = true;
+
+Task stopTask = Task.Run(() =>
+{
+    Console.ReadKey();
+    return true;
+});
+
+Console.CursorVisible = false;
+Console.Clear();
+
+for (int i = 1; !stopTask.IsCompleted; i += 2)
+{
+    if (sign) pi += 4d / i;
+    else pi -= 4d / i;
+    sign = !sign;
+
+    Console.SetCursorPosition(0, 0);
+    Console.Write(pi);
+}
+
+Console.CursorVisible = true;
---
+++ 018_MultipleAsync/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> primes = new List<int>();
+List<Task<List<int>>> calcs = new List<Task<List<int>>>();
+
+for (int i = 5; i <= 10; i++)
+{
+    calcs.Add(CalculatePrimesInRange((i - 1) * 100, i * 100));
+}
+foreach (Task<List<int>> calc in calcs)
+{
+    primes.AddRange(await calc);
+}
+Console.Clear();
+foreach (int prime in primes)
+{
+    Console.Write(prime);
+}
+
+async Task<List<int>> CalculatePrimesInRange(int min, int max)
+{
+    List<int> primes = new List<int>();
+    bool isPrime;
+
+    for (int i = min; i < max; i++)
+    {
+        isPrime = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) Console.Write(i + " ");
+    }
+    return primes;
+}
