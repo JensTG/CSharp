2c8bc584a6129b9f47f2b0a4db0cecc1906883bf
Jens <jens@cetg.dk>
2024-01-15 21:56:18 +0100

    008 Completed

---
+++ 019_ProjectEuler/009_PythagoreanTriplet.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ProjectEuler
+{
+    using static Funcs;
+    using static System.Console;
+    class PythagoreanTriplet
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
